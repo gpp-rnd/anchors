e22b46daf24954de667ca7c36bca907059d314ea
PeterDeWeirdt <petedeweirdt@gmail.com>
2020-09-21 21:03:03 -0400

    Bump version: 0.1.0 → 0.2.0

--- anchors/__init__.py
+++ anchors/__init__.py
@@ -2,4 +2,4 @@
 print('hello world')
 __author__ = """Peter Carl DeWeirdt"""
 __email__ = 'petedeweirdt@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/gpp-rnd/anchors',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
 )
